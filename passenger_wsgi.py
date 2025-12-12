223fcc68b55206f7ce7f7fa54147b858f59855ed
tndkhoa-evi <khoa.tnd@evi.vn>
2025-12-12 16:29:47 +0700

    Simplify passenger_wsgi.py for MatBao hosting

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,21 +1,9 @@
-"""
-Passenger WSGI entry point for DirectAdmin Python hosting.
-"""
 import os
 import sys
 
-# Add the project directory to the path
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-if CURRENT_DIR not in sys.path:
-    sys.path.insert(0, CURRENT_DIR)
+sys.path.insert(0, os.path.dirname(__file__))
 
-# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Initialize Django
-import django
-django.setup()
-
-# Get WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
